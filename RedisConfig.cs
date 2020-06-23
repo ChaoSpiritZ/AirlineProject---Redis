2f5899fbc939c4118ac25a7bcee40e146e038809
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2020-06-23 15:59:40 +0300

    Add files via upload

---
+++ RedisConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineProject
+{
+    static class RedisConfig
+    {
+        public const string GetDepartingFlightsFullData = "GetDepartingFlightsFullData";
+        public const string GetLandingFlightsFullData = "GetLandingFlightsFullData";
+    }
+}
